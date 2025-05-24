b7624df054b90f0c1c0a03c51259f4707aae9ead
Alexandre TAHI <alexandretahi7@gmail.com>
2025-05-24 06:47:12 +0200

    Add click sound and haptic feedback functionality to TimerSelect while replacing expo-av with expo-audio.

--- components/domain/TimerSelect.tsx
+++ components/domain/TimerSelect.tsx
@@ -4,11 +4,12 @@ import {useColorScheme} from '@/utils/hooks/useColorScheme';
 import {TimerPicker} from "react-native-timer-picker";
 import MaskedView from "@react-native-masked-view/masked-view"; // for transparent fade-out
 import {LinearGradient} from "expo-linear-gradient"; // or `import LinearGradient from "react-native-linear-gradient"`
-import {Audio} from "expo-av"; // for audio feedback (click sound as you scroll)
-import * as Haptics from "expo-haptics";
 import {FontHelper} from "@/utils/helpers/fontHelper";
 import {FontWeightEnum} from "@/utils/enums";
 import {ThemedView} from "@/components/base/ThemedView";
+import {clickAudioSource} from "@/utils/constants";
+import {useAudioPlayer} from 'expo-audio';
+import {UIHelper} from "@/utils/helpers/UIHelper";
 
 type Props = {
     defaultValue?: TimerValue;
@@ -18,6 +19,7 @@ type Props = {
 export function TimerSelect({defaultValue, onChange}: Props) {
     const [time, setTime] = useState<TimerValue>(defaultValue ?? {hours: 0, minutes: 0, seconds: 0});
     const colorScheme = useColorScheme() ?? 'light';
+    const audioPlayer = useAudioPlayer(clickAudioSource); // TODO: Find how to use this to play sound at each change
 
     const updateTime = (time: TimerValue) => {
         setTime(time);
@@ -35,14 +37,14 @@ export function TimerSelect({defaultValue, onChange}: Props) {
                 hourLabel="hrs"
                 minuteLabel="min"
                 secondLabel="sec"
-                clickSoundAsset={require("@/assets/sounds/click.mp3")}
-                Audio={Audio}
                 LinearGradient={LinearGradient}
-                Haptics={Haptics}
                 MaskedView={MaskedView}
                 initialValue={defaultValue}
                 onDurationChange={updateTime}
                 disableInfiniteScroll={true}
+                pickerFeedback={() => {
+                    UIHelper.hapticImpact('selection');
+                }}
                 styles={{
                     theme: colorScheme,
                     backgroundColor: "transparent",
--- utils/constants.ts
+++ utils/constants.ts
@@ -2,3 +2,5 @@ import {TimerValue} from "@/utils/interfaces";
 
 export const defaultWorkTime: TimerValue = {hours: 0, minutes: 30, seconds: 0};
 export const defaultBreakTime: TimerValue = {hours: 0, minutes: 10, seconds: 0};
+
+export const clickAudioSource = require("@/assets/sounds/click.mp3");
