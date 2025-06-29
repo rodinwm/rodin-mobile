6009e80fe00e61f01642b08881b2ef6c3c58b68b
Alexandre TAHI <62557152+alexxtahi@users.noreply.github.com>
2025-06-30 01:48:15 +0200

    Updated `ci_post_clone.sh` to install Node.js dependencies, adjusted directory navigation, and improved pod cleaning steps for better structure

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -3,10 +3,14 @@
 echo "🔧 [CI] Using Node at: $(which node)"
 echo "📁 [CI] Cleaning previous pods..."
 
-cd "$(dirname "$0")/.." || exit
-rm -rf Pods Podfile.lock *.xcworkspace
+cd "$(dirname "$0")/../.." || exit
+npm install
+
+cd ios || exit
+rm -rf Pods Podfile.lock
 
 echo "📦 [CI] Installing pods..."
+
 pod install --repo-update
 
 echo "✅ [CI] Pods installed successfully"
