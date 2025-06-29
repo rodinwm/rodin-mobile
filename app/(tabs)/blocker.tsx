7deb00ee18fe13a8912bf0593d810e4f62d4ee65
Alexandre TAHI <62557152+alexxtahi@users.noreply.github.com>
2025-06-29 14:27:55 +0200

    Added optional `screenName` prop to `ScreenTemplate`, optimized imports in `blocker.tsx`, and removed `dev` branch trigger from iOS build workflow

--- app/(tabs)/blocker.tsx
+++ app/(tabs)/blocker.tsx
@@ -1,24 +1,10 @@
-import {
-    LucideIcon,
-    ScreenTemplate,
-    SystemAppIcon,
-    ThemedButton,
-    ThemedText,
-    ThemedTextInput,
-    ThemedView
-} from '@/components';
-import React, {useState, useEffect} from "react";
-import {useRouter} from "expo-router";
-import {FlatList} from "react-native";
-import {SystemApp} from "@/utils/interfaces";
+import {LucideIcon, ScreenTemplate, ThemedButton, ThemedText, ThemedView} from '@/components';
+import React, {useEffect, useState} from "react";
 import {NativeAppBlockerView} from "@/components/layouts/NativeAppBlockerView";
 import {AppBlockerService} from "@/utils/services/appBlockerService";
 
 export default function Page() {
-    const router = useRouter();
-    const [selectedApps, setSelectedApps] = useState<SystemApp[]>([]);
     const [isAppBlockerInit, setIsAppBlockerInit] = useState(AppBlockerService.isInit());
-    const [searchedApp, setSearchedApp] = useState('');
 
     const initAppBlocker = () => {
         AppBlockerService.init().then((isInit) => setIsAppBlockerInit(isInit));
