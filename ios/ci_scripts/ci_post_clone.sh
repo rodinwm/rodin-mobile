c03f882b27f2dae01b274cd67fe90c3c520ad2bb
Alexandre TAHI <62557152+alexxtahi@users.noreply.github.com>
2025-06-30 00:55:31 +0200

    Updated `ci_post_clone.sh` to clean Pods and related files from the root directory instead of the `ios` subdirectory

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "👉 [CI] Cleaning Pods..."
 cd ios || exit
-rm -rf Pods Podfile.lock *.xcworkspace
+rm -rf ../Pods ../Podfile.lock ../*.xcworkspace
 
 echo "👉 [CI] Installing Pods..."
 pod install --repo-update
