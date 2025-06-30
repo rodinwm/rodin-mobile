fd2522f5e88fb7160d509ab5100a9d42e3ebeae8
Alexandre TAHI <62557152+alexxtahi@users.noreply.github.com>
2025-06-30 02:27:40 +0200

    Enhanced `ci_post_clone.sh` by adding .npmrc setup with GitHub token, restoring npm installation, and refining logging messages

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -5,19 +5,30 @@ cd "$(dirname "$0")/../.." || exit 1
 
 echo "[CI] Installing Node"
 brew install node
-echo "[CI] Node installed successfully"
+echo "[CI] ✅ Node installed successfully"
 echo "[CI] Using Node at: $(which node)"
 
-#echo "Installing npm dependencies"
-#npm install
-#echo "npm dependencies successfully installed"
+echo "[CI] Setting up .npmrc for GitHub Packages"
+
+if [ -z "$NODE_AUTH_TOKEN" ]; then
+  echo "[ERROR] NODE_AUTH_TOKEN is not set. Aborting npm install."
+  exit 1
+fi
+
+# Prepare .npmrc
+sed -i.bak "s|^#*//npm.pkg.github.com|//npm.pkg.github.com|" .npmrc
+sed -i '' "s|NODE_AUTH_TOKEN|$NODE_AUTH_TOKEN|g" .npmrc
+echo "[CI] ✅ .npmrc updated with GitHub token"
+
+echo "[CI] Installing npm dependencies"
+npm install
+echo "[CI] ✅ npm dependencies successfully installed"
 
 echo "[CI] Cleaning previous pods"
 cd ios || exit 1
 rm -rf Pods Podfile.lock
-echo "[CI] Previous pods cleaned"
+echo "[CI] ✅ Previous pods cleaned"
 
 echo "[CI] Installing pods"
 pod install --repo-update
-
 echo "[CI] ✅ Pods installed successfully"
